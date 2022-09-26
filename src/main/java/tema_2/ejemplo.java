39ac7b9615eef7d6b62e08132cd24965c68320da
alvaro <alvaromarflo@gmail.com>
2022-09-26 13:52:03 +0200

    muestra si a es mayor que b

--- src/main/java/tema_2/ejemplo.java
+++ src/main/java/tema_2/ejemplo.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tema_2;
 
 /**
@@ -11,5 +7,10 @@ package tema_2;
 public class ejemplo {
     public static void main(String[] args) {
         System.out.println("Alvarito");
+        int a = 10;
+        int b = 20;
+        boolean aMayorB = a>b;
+        
+        System.out.println(aMayorB);
     }
 }
