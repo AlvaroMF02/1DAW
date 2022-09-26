97c6c770577c661723cd65921c223cb48be9212d
alvaro <amarflo1406@g.educaand.es>
2022-09-26 20:04:12 +0200

    ejer 5 terminado

--- src/main/java/tema_2/ejercicios_de_introduccion/ejer5.java
+++ src/main/java/tema_2/ejercicios_de_introduccion/ejer5.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tema_2.ejercicios_de_introduccion;
 
 /**
@@ -9,5 +5,33 @@ package tema_2.ejercicios_de_introduccion;
  * @author alvaro
  */
 public class ejer5 {
-    
+    public static void main(String[] args) {
+        //VARIABLES
+        boolean  verdadero = true;
+        boolean  falso = false;
+        
+        String resultados = """
+                            RESULTADOS
+                            verdadero && verdadero = %b
+                            verdadero && falso = %b
+                            falso && verdadero = %b
+                            falso && falso = %b
+                            verdadero || verdadero = %b
+                            verdadero || falso = %b
+                            falso || verdadero = %b
+                            falso || falso = %b
+                            
+                            """
+                .formatted(verdadero&&verdadero,verdadero&&falso,falso&&verdadero,falso && falso
+                ,verdadero || verdadero,verdadero || falso,falso || verdadero,falso || falso);
+
+        //RESULTADOS
+        System.out.println(resultados);
+        
+        System.out.println(" !verdadero =" + !verdadero);
+        System.out.println(" !falso =" + !falso);
+        
+        
+        
+    }
 }
