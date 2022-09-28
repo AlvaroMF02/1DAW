be6f4c1db1d5d542b958265f820516a177d68fdd
alvaro <alvaromarflo@gmail.com>
2022-09-28 13:26:32 +0200

    ejer8 corregido

--- src/main/java/tema_2/ejercicios_secuenciales/ejer8.java
+++ src/main/java/tema_2/ejercicios_secuenciales/ejer8.java
@@ -14,35 +14,29 @@ public class ejer8 {
 
         //VARIABLES
         double presupuesto;
-        double ginecologia;     //40%
-        double traumatologia;   //30%
-        double pediatria;       //30%
+        double presupGinecologia;     //40%
+        double presupTraumatologia;   //30%
+        double presupPediatria;       //30%
 
         System.out.println("Indique el presupuesto de este año para el hospital");
         presupuesto = teclado.nextDouble();
 
         //OPERACIONES
-        ginecologia = presupuesto * 0.4;
-        traumatologia = presupuesto * 0.3;
-        pediatria = presupuesto * 0.3;
+        presupGinecologia = presupuesto * 0.4;
+        presupTraumatologia = presupuesto * 0.3;
+        presupPediatria = presupuesto * 0.3;
 
-        /* HACIENDOLO CON ESTO ME DA ERROR
+        //RESULTADO
        
         String resultados = """
                            
                            RESULTADOS
-                           Ginecología (40%) recibe %.2f
-                           Traumatología (30%) recibe %.2f 
-                           Pediatría (30%) recibe %.2f
-                           """.formatted(ginecologia, traumatologia, pediatria);
+                           Ginecología recibe %.2f
+                           Traumatología recibe %.2f 
+                           Pediatría recibe %.2f
+                           """.formatted(presupGinecologia, presupTraumatologia, presupPediatria);
 
         System.out.println(resultados);
-         */
-        
-        //RESULTADO
-        System.out.println("Ginecología (40%) recibe "+ginecologia);
-        System.out.println("Traumatología (30%) recibe "+traumatologia);
-        System.out.println("Pediatría (30%) recibe "+pediatria);
-        
+         
     }
 }
