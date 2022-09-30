a362611bfa1df807ac9851653858d346b6b7598a
alvaro <alvaromarflo@gmail.com>
2022-09-30 10:15:14 +0200

    .

--- src/main/java/tema_2/explicaciones/joption.java
+++ src/main/java/tema_2/explicaciones/joption.java
@@ -1,6 +1,5 @@
 package tema_2.explicaciones;
 
-import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 /**
@@ -8,22 +7,32 @@ import javax.swing.JOptionPane;
  * @author alvaro
  */
 public class joption {
+
     public static void main(String[] args) {
-        
-        //LECTURA DE DATOS CON J OPTION
-                
+
         //VARIABLE
-        String datos;
-      
+        String nombre;
+        double peso;
+        String pesoString;
+        int edad;
+        String edadString;
+
         
-        //LEER DATOS CON JOPTION
-        datos = JOptionPane.showInputDialog("Escribe tu nombre");
-        
+        //LEER DATOS CON JOPTION STRING
+        nombre = JOptionPane.showInputDialog("Escribe tu nombre");
         //MOSTRAR DATOS
-        JOptionPane.showMessageDialog(null, "Tu nombre es "+ datos);
+        JOptionPane.showMessageDialog(null, "Tu nombre es " + nombre);
+
         
+        //LEER DATOS NUMERICOS DECIMALES
+        pesoString = JOptionPane.showInputDialog("Escribe tu peso");
+        peso=Double.parseDouble(pesoString);                                //TRANSFORAR STRING EN DOUBLE
+        JOptionPane.showMessageDialog(null, "Pesas " + peso*10); //ASI SE PUEDEN HACER OPERACIONES
         
+        //LEER DATOS NUMERICOS ENTEROS
+        edadString = JOptionPane.showInputDialog("Escribe tu edad");
+        edad=Integer.parseInt(edadString);
+        JOptionPane.showMessageDialog(null, "Tienes " + edad);
         
-      
     }
 }
