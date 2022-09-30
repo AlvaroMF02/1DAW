5b050aed88e3114bd8e1dd63ccb3d5aab4d48f95
alvaro <alvaromarflo@gmail.com>
2022-09-30 11:15:34 +0200

    terminar secuenciales

---
+++ src/main/java/tema_2/ejercicios_secuenciales/ejer1.java
@@ -0,0 +1,22 @@
+package tema_2.ejercicios_secuenciales;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author alvaro
+ */
+public class ejer1 {
+    public static void main(String[] args) {
+        //VARIABLES
+        String gradosCent;
+        double gradosFareng;
+        final int DIFERENCIAGRADOS = 32;
+        
+        
+        
+        gradosCent = JOptionPane.showInputDialog("Indica los grados Centigrados");
+        gradosFareng=Double.parseDouble(gradosCent);
+        JOptionPane.showMessageDialog(null, gradosCent +" grados son " + gradosFareng*DIFERENCIAGRADOS + " Farenheit");
+    }
+}
---
+++ src/main/java/tema_2/ejercicios_secuenciales/ejer3.java
@@ -0,0 +1,33 @@
+package tema_2.ejercicios_secuenciales;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author alvaro
+ */
+public class ejer3 {
+    public static void main(String[] args) {
+        //VARIABLES
+        float nota1;
+        String nota1Str;
+        float nota2;
+        String nota2Str;
+        float nota3;
+        String nota3Str;
+        
+        nota1Str = JOptionPane.showInputDialog("Escribe la nota nº 1");
+        nota1=Float.parseFloat(nota1Str);
+        
+        nota2Str = JOptionPane.showInputDialog("Escribe la nota nº 2");
+        nota2=Float.parseFloat(nota2Str);
+        
+        nota3Str = JOptionPane.showInputDialog("Escribe la nota nº 3");
+        nota3=Float.parseFloat(nota3Str);
+        
+        
+        //OPERACIONES
+        
+        
+    }
+}
