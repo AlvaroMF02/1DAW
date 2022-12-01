9192daa148e04bde78d77b33220e974da7c7498a
alvaro <amarflo1406@g.educaand.es>
2022-12-01 19:00:08 +0100

    miau

--- src/main/java/tema_5/EjerciciosDeClase/Ejer02.java
+++ src/main/java/tema_5/EjerciciosDeClase/Ejer02.java
@@ -7,32 +7,31 @@ import java.util.Scanner;
  * @author alvaro
  */
 public class Ejer02 {
-    
+
     public static void main(String[] args) {
-        
+
         Scanner teclado = new Scanner(System.in);
-        
+
         //VARIABLES
         int cantiPerson;           //CONTROLAR EXCEPCIONES
         int total = 0;
-        
-        
-        System.out.println("¿Cuántas personas hay?");
+        double media;
+
+        System.out.println("¿Cuántas personas són?");
         cantiPerson = teclado.nextInt();
-        
-        int [] altura = new int[cantiPerson];
-        
+
+        int[] altura = new int[cantiPerson];
+
         for (int i = 0; i < altura.length; i++) {
-            System.out.println("Indica la altura de la " + i + "º persona:");
-            
-            altura[i]=teclado.nextInt();
+            System.out.println("Indica la altura de la " + (i + 1) + "º persona:");
+
+            altura[i] = teclado.nextInt();
             i = Math.abs(i);
-        }
-        
-        for (int i = 0; i < altura.length; i++) {
             total += altura[i];
         }
-        
-        
+
+        //HACER LA MEDIA
+        media = total / cantiPerson;
+        System.out.println(media);      //LA MEDIA NO SALE EXACTA
     }
 }
