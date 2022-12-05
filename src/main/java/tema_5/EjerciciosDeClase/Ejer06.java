292e0d1e7e48103bdd40f2a49695f41c358c1217
alvaro <amarflo1406@g.educaand.es>
2022-12-05 14:37:57 +0100

    meow

--- src/main/java/tema_5/EjerciciosDeClase/Ejer06.java
+++ src/main/java/tema_5/EjerciciosDeClase/Ejer06.java
@@ -7,18 +7,17 @@ import java.util.Arrays;
  * @author alvaro
  */
 public class Ejer06 {
-    
+
     public static void main(String[] args) {
-        
+
         //ARRAYS
-        int [] array1 = {1,2,3};
-        int [] array2 = {1,2,3};
-        int [] array3 = {1,2,3,4};
-        int [] array4 = {1,2,3};
-        int [] array5 = {1,2,3};
-        int [] array6 = {1,2,2};
-        
-        
+        int[] array1 = {1, 2, 3};
+        int[] array2 = {1, 2, 3};
+        int[] array3 = {1, 2, 3, 4};
+        int[] array4 = {1, 2, 3};
+        int[] array5 = {1, 2, 3};
+        int[] array6 = {1, 2, 2};
+
         //USANDO METODO EQUALS DE ARRAYS
         System.out.println("Comparacion array 1 y 2 : " + Arrays.equals(array1, array2));
         System.out.println("Comparacion array 1 y 3 : " + Arrays.equals(array1, array3));
@@ -27,17 +26,31 @@ public class Ejer06 {
         System.out.println("Comparacion array 1 y 6 : " + Arrays.equals(array1, array6));
 
         
+        //COMPARAR CON MI METODO
+        boolean comparacion = compararArrays(array1, array2);
+        System.out.println("Comparacion 1 y 2: " + comparacion);
         
+        comparacion = compararArrays(array1, array3);
+        System.out.println("Comparacion 1 y 3: " + comparacion);
     }
-    
+
     //COMPARAR CADA ARRAY
-    public boolean compararArrays(int [] aux){
+    public static boolean compararArrays(int[] aux, int[] aux2) {
         boolean iguales = false;
-        
-        
-        
-        
+
+        if (aux.length != aux2.length) {
+            return false;
+        } else {
+            for (int i = 0; i < aux.length; i++) {
+                if (aux[i] != aux2[i]) {
+                    return false;
+                } else {
+                    iguales = true;
+                }
+            }
+        }
+
         return iguales;
     }
-    
+
 }
---
+++ src/main/java/tema_5/EjerciciosDeClase/Ejer07.java
@@ -0,0 +1,58 @@
+package tema_5.EjerciciosDeClase;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author alvaro
+ */
+public class Ejer07 {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        Random alea = new Random();
+
+        System.out.println("Indique el tamaño de los arrays");
+        int tamanyo = teclado.nextInt();
+
+        int[] lista1 = new int[tamanyo];
+        int[] lista2 = new int[tamanyo];
+        int[] multiplicado;
+
+        //RELLENAR ARRAYS
+        for (int i = 0; i < tamanyo; i++) {
+            lista1[i] = alea.nextInt(1, 100 + 1);
+            lista2[i] = alea.nextInt(1, 100 + 1);
+        }
+
+        mostrar(lista1);
+        System.out.println("");
+        mostrar(lista2);
+        System.out.println("");
+        multiplicado = multiplicar(lista1, lista2);
+
+        mostrar(multiplicado);
+
+    }
+
+    //MULTIPLICAR
+    public static int[] multiplicar(int[] aux1, int[] aux2) {
+        int[] listaMulti = new int[aux1.length];
+
+        for (int i = 0; i < aux1.length; i++) {
+            listaMulti[i] = aux1[i] * aux2[i];
+        }
+
+        return listaMulti;
+    }
+
+    //MOSTRAR ARRAYS
+    public static void mostrar(int[] aux) {
+        for (int i = 0; i < aux.length; i++) {
+            System.out.print(aux[i] + ", ");
+        }
+
+    }
+}
