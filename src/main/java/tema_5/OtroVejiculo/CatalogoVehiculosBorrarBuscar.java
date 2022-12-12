86b7c742623f7d6aa4a73624a1e0afbe216eb232
alvaro <amarflo1406@g.educaand.es>
2022-12-12 13:48:31 +0100

    metodo añadir vehiculo

--- src/main/java/tema_5/OtroVejiculo/CatalogoVehiculosBorrarBuscar.java
+++ src/main/java/tema_5/OtroVejiculo/CatalogoVehiculosBorrarBuscar.java
@@ -1,28 +1,31 @@
 package tema_5.OtroVejiculo;
 
+import java.util.Arrays;
+
 /**
  *
  * @author alvaro
  */
 public class CatalogoVehiculosBorrarBuscar {
-    
+
     private int numeroVehiculo;
     private Vehiculo[] listaVehiculo;
 
     public CatalogoVehiculosBorrarBuscar(int tamanyo) {
-        
+
         tamanyo = Math.abs(tamanyo);
-        
+
         this.numeroVehiculo = tamanyo;
         this.listaVehiculo = new Vehiculo[tamanyo];         //CREAR LA ESTRUCTURA
-        
+
         //DAR VALORES
         for (int i = 0; i < this.listaVehiculo.length; i++) {
             this.listaVehiculo[i] = new Vehiculo();
         }
-        
+
     }
 
+    //DICE EL NUMERO DE VEHICULOS QUE HAY EN EL CATALOGO
     public int getNumeroVehiculo() {
         return numeroVehiculo;
     }
@@ -30,36 +33,82 @@ public class CatalogoVehiculosBorrarBuscar {
     public Vehiculo[] getListaVehiculo() {
         return listaVehiculo;
     }
-    
-    
-    public void mostrarCatalogo(){
+
+
+    public void mostrarCatalogo() {
         for (Vehiculo v : listaVehiculo) {
             System.out.println(v);
         }
-        
+
     }
-    
-    public int buscarVehiculo(Vehiculo v){
+
+    public int buscarVehiculo(Vehiculo v) {
+        //BUSQUEDA SECUENCIAL
         for (int i = 0; i < this.listaVehiculo.length; i++) {
             if (v.equals(listaVehiculo[i])) {
                 return i;
             }
         }
-        
+
         return -1;
     }
-    
-    
-    
-    public void borrarVehiculo(Vehiculo v){
-        
+
+    public boolean borrarVehiculo(Vehiculo v) {
+
         int pos = buscarVehiculo(v);
-        
+
         if (pos >= 0) {
             this.listaVehiculo[pos] = null;
+            this.numeroVehiculo--;
+            return true;
         }
         
+        return false;
     }
-    
-    
+
+    //CREAR UNA COPIA CON MAS GRANDE PARA AÑADIR OTRO VEHICULO
+    //SI HAY HUECO EN EL ARRAY SE INSERTA UN VEHICULO NUEVO
+    public void anadirVehiculo(Vehiculo v) {
+        
+        //HAY HUECO
+        if (this.numeroVehiculo < this.listaVehiculo.length) {
+            //BUSCAR EL ESPACIO EN NULL
+            for (int i = 0; i < this.listaVehiculo.length; i++) {
+                //SI HAY ESPACIO SE GUARDA EN I
+                if (this.listaVehiculo[i] == null) {
+                    this.listaVehiculo[i] = v;
+                    this.numeroVehiculo++;
+                    System.out.println("Se guarda vehiculo en: " + i);
+                    break;
+                }
+            }
+        }else{  //CASO EN EL QUE EL ARRAY ESTA LLENO
+            
+            this.listaVehiculo = Arrays.copyOf(this.listaVehiculo, ++this.numeroVehiculo);    //CREAO UNA COPIA DEL ARRAY PERO CON UN ESPACIO MAS QUE SE QUEDARÁ A NULL
+            
+            this.listaVehiculo[this.numeroVehiculo - 1] = v;
+            
+            System.out.println("Se guarda vehiculo nuevo");
+        }
+        
+        
+    }
+
+    public String toString() {
+        String tmp = "";
+
+        
+        
+        for (Vehiculo v : listaVehiculo) {
+            
+            if (v!= null) {
+                tmp += v.toString() + "\n";         //DA ERROR NULL POINTER
+            }   
+            
+            tmp += v.toString() + "\n";
+        }
+
+        return tmp;
+    }
+
 }
--- src/main/java/tema_5/OtroVejiculo/EjemploVehiculo.java
+++ src/main/java/tema_5/OtroVejiculo/EjemploVehiculo.java
@@ -9,14 +9,36 @@ public class EjemploVehiculo {
     public static void main(String[] args) {
         
         CatalogoVehiculosBorrarBuscar catalogo = new CatalogoVehiculosBorrarBuscar(10);
+        System.out.println("Numero de vehiculos del catalogo " + catalogo.getNumeroVehiculo());
+        
+        
         
         
         catalogo.mostrarCatalogo();
         System.out.println("*******************************");
         
+        System.out.println("Numeros de vehiculos antes de borrar uno: " + catalogo.getNumeroVehiculo());
+        
         catalogo.borrarVehiculo(catalogo.getListaVehiculo()[6]);
         catalogo.mostrarCatalogo();
         
+        System.out.println("Numeros de vehiculos despues de borrar uno: " + catalogo.getNumeroVehiculo());
+
+//        String resultado = catalogo.toString();
+//        System.out.println(resultado);
+        System.out.println("AÑADIR VEHICULO EN UNA POSICION EN MEDIO");
+        Vehiculo aux = new Vehiculo();
+        catalogo.anadirVehiculo(aux);
+        aux.setBastidor("12345");
+        catalogo.mostrarCatalogo();
+        System.out.println("Numeros de vehiculos: " + catalogo.getNumeroVehiculo());
         
+        
+        System.out.println("AÑADIR VEHICULO AL FINAL");
+        Vehiculo aux2 = new Vehiculo();
+        aux2.setBastidor("99999");
+        catalogo.anadirVehiculo(aux2);
+        catalogo.mostrarCatalogo();
+        System.out.println("Numeros de vehiculos: " + catalogo.getNumeroVehiculo());
     }
 }
