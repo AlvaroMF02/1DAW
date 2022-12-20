da07ff716db1d40d9a58c2dfe841d24bc09609ce
alvaro <amarflo1406@g.educaand.es>
2022-12-20 17:15:20 +0100

    acabar los metodos de busqueda y ordenacion, hacer las pruebas y añadir excepciones

--- src/main/java/tema_5/EjerciciosDeClase/Ejer24/ListaReproduccion.java
+++ src/main/java/tema_5/EjerciciosDeClase/Ejer24/ListaReproduccion.java
@@ -1,6 +1,7 @@
 package tema_5.EjerciciosDeClase.Ejer24;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -8,23 +9,96 @@ import java.util.List;
  * @author alvaro
  */
 public class ListaReproduccion {
-    
-    List <Cancion> lista;
+
+    List<Cancion> lista;
 
     //CONSTRUCTOR
-    public ListaReproduccion(int tamanio) {
-        
-        tamanyo = Math.abs(tamanyo);
+    public ListaReproduccion() {
+        this.lista = new ArrayList<>();
+    }
 
-        this.lista = new ArrayList<>(tamanyo);
+    //DEVUELVE EL NUMERO DE CANCIONES
+    public int numeroCanciones() {
+        int cantidad = 0;
 
-        //RELLENAR EL ARRAY CON LOS VALORES ALEATORIOS DE LA CLASE CLIENTE
-        for (int i = 0; i < this.lista.size(); i++) {
-            this.lista.add(new Cliente());
+        cantidad = this.lista.size();
+
+        return cantidad;
+    }
+
+    //MUESTRA SI LA LISTA ESTA VACIA
+    public boolean estaVacia() {                                                //COMPROBARLO
+        boolean vacia = false;
+
+        if (numeroCanciones() == 0) {
+            vacia = true;
         }
+
+        return vacia;
+    }
+
+    //MUESTRA LA CANCION QUE INDIQUES POR EL ID                                 //COMPROBARLO
+    public void escucharCancion(int id) {
+        if (!estaVacia()) {
+            this.lista.get(id);
+        }
+    }
+
+    //CAMBIA LA CANCION POR UNA NUEVA QUE SE PASA                               //COMPROBARLO
+    public void cambiarCancion(int posicion, Cancion nueva) {
+        this.lista.set(posicion, nueva);
+    }
+    
+    //AÑADE LA CANCION QUE LE PASE                                              //COMPROBARLO
+    public void grabarCancion(Cancion nueva) {
+        this.lista.add(nueva);
     }
     
     
+    //ELIMINA LA CANCION QUE INDIQUES                                           //COMPROBARLO INDEX OUT OF BOUND
+    public void eliminaCancion(int id){
+        this.lista.remove(id);
+    }
+    
+    //ELIMINA LA CANCION QUE INDIQUES PERO CON OBJETO                           //COMPROBARLO INDEX OUT OF BOUND
+    public void eliminaCancion(Cancion c){
+        this.lista.remove(c);
+    }
+    
+    //MUESTRA LOS NOMBRES DE LAS CANCIONES                                      //COMPROBARLO
+    public String imprimirLista (ListaReproduccion tmp){
+        
+        String nombres = "";
+        
+        for (Cancion cancion : lista) {
+            nombres += cancion.getNombre() + "\n";
+        }
+        
+        return nombres;
+    }
+    
+    //PASA LA POSICION DE LA CANCION QUE BUSQUES
+    public int buscarCancion(Cancion c){                                        //COMPROBAR
+        
+        int posicion = 0;
+        
+        posicion = this.lista.indexOf(c);
+        
+        return posicion;
+    }
+    
+    //ORDENA LA LISTA POR EL NOMBRE                                             //TERMINARLO
+    public void ordenarListaNombre(){
+        Collections.sort(this.lista, c);
+    }
+    
+    //ORDENA LA LISTA POR EL AUTOR                                              //TERMINARLO
+    public void ordenarListaAutor(){
+        Collections.sort(lista, c);
+    }
     
     
+    //BUSCAR CANCIONES POR LISTA
+    //BUSCAR CANCIONES POR OBJETO
+    //BUSCAR CANCIONES POR CRITERIOCOMPARATOR
 }
